fcf1e0c07aa6e027c213fb80e7af0e9c2feb5154
Cedric Krause <ced.krause@gmail.com>
2024-12-01 08:10:11 +0100

    Day01 - Solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,51 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (leftNumbers, rightNumbers) = getLists(input)
+
+        val differences = leftNumbers.sorted()
+            .zip(rightNumbers.sorted())
+            .map { abs(it.first - it.second) }
+
+        return differences.reduce { acc, next -> acc + next }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var similarityScore = 0
+        val (leftNumbers, rightNumbers) = getLists(input)
+
+        leftNumbers.forEach { left ->
+            rightNumbers.forEach { right ->
+                if (left == right) similarityScore += left
+            }
+        }
+
+        return similarityScore
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    fun part2_2(input: List<String>): Int {
+        val (leftNumbers, rightNumbers) = getLists(input)
+
+        return leftNumbers.fold(0) { acc, next ->
+            acc + rightNumbers.fold(0) { innerAcc, innerNext -> if (innerNext == next) innerAcc + innerNext else innerAcc }
+        }
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
+    check(part2_2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+    part2_2(input).println()
+}
+
+private fun getLists(input: List<String>): Pair<List<Int>, List<Int>> {
+    val rawPairs = input.map { line -> line.split(" ").filter { it.isNotBlank() }.map { it.trim().toInt() } }
+    return Pair(rawPairs.map { it.first() }, rawPairs.map { it.last() })
 }
