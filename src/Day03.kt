a45ef4135d5b9def396a9279488f2fb336bcd43d
Cedric Krause <hello.cedric@proton.me>
2024-12-03 20:07:18 +0100

    Day03 - Solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,18 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val pattern = Regex("mul\\([0-9]{1,3},[0-9]{1,3}\\)")
+        val matches = input.map { pattern.findAll(it) }
+            .flatMap { matchResults -> matchResults.map { it.value } }
+
+        val products = matches.map { it.split("(", ",", ")") }.map { it[1].toInt() * it[2].toInt() }
+        return products.reduce { acc, next -> acc + next }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val sections = input.joinToString("")
+            .split("do()")
+            .map { it.split("don't()").first() }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+        return part1(sections)
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 161)
+    check(part2(testInput) == 48)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day03")
